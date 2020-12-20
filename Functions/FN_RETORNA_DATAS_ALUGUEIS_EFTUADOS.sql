4ea5b4aefce3d019fb91e45fa21d3fb1f4353f84
Fts99 <47905996+Fts99@users.noreply.github.com>
2020-12-20 20:24:44 -0300

    Ajustes

--- Functions/FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS.sql
+++ Functions/FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS.sql
@@ -1,59 +1,72 @@
 CREATE OR REPLACE FUNCTION FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS(P_ID_PRODUTO UUID)
-RETURNS TEXT AS $$	
+    RETURNS TEXT AS $$
 declare DIA_INICIO DATE;
-		QTD_DIAS INT;
-		v_alugueis text[];
-		v_aluguel text;
-		v text;
-declare dt_aluguel_cursor cursor(data timestamp)
-							for select to_date(to_char(DATA_INICIO,'YYYY-MM-DD'),'YYYY-MM-DD'), extract(day from (DATA_FIM - DATA_INICIO))
-							from alugueis A
-							where A.ID_PRODUTO = UUID(P_ID_PRODUTO)
-							and to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD') >= current_date;
-begin 
-	DROP TABLE IF EXISTS DIAS_ALUGUEL;
-	CREATE TEMPORARY TABLE DIAS_ALUGUEL(
-	DT_ALUGUEL DATE
-	);
-	
-	open dt_aluguel_cursor(current_timestamp);
-	loop
-		fetch dt_aluguel_cursor into DIA_INICIO, QTD_DIAS;
-		exit when not found;
-		
-		INSERT INTO DIAS_ALUGUEL(DT_ALUGUEL)
-		VALUES(DIA_INICIO);
-		LOOP			
-			IF QTD_DIAS <= 0 THEN
-				EXIT;
-			END IF;
-			DIA_INICIO = DIA_INICIO + 1;
-			INSERT INTO DIAS_ALUGUEL(DT_ALUGUEL)
-			VALUES(DIA_INICIO);
-			QTD_DIAS = QTD_DIAS - 1;
-			--DIA_INICIO = DIA_INICIO || ',' || DIA_INICIO + 1;
-		END LOOP;
-	END LOOP;
-	
-	close dt_aluguel_cursor;	
-	
-	v_alugueis = (SELECT ARRAY_AGG(DISTINCT DT_ALUGUEL)
-					FROM DIAS_ALUGUEL);
-					
-	IF v_alugueis IS NULL THEN
-		RETURN '';
-	END IF;
-	
-	FOREACH v IN array v_alugueis
-	LOOP
-		IF v_aluguel IS NOT NULL THEN 
-			v_aluguel = v_aluguel || ',' || v;
-			
-		ELSE
-			v_aluguel = v;
-		END IF;
-		
-	end loop;
-	RETURN v_aluguel;
+        DIA_FIM DATE;
+        v_alugueis_INCIO text[];
+        v_alugueis_FIM text[];
+        v_aluguel_INICIO text;
+        v_aluguel_FIM text;
+        v text;
+    declare dt_aluguel_cursor cursor(data timestamp)
+            for select to_date(to_char(DATA_INICIO,'YYYY-MM-DD'),'YYYY-MM-DD'), to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD')--, extract(day from (DATA_FIM - DATA_INICIO))
+                from alugueis A
+                where A.ID_PRODUTO = UUID(P_ID_PRODUTO)
+                and to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD') >= current_date
+                order by id_aluguel;
+begin
+    DROP TABLE IF EXISTS DT_INICIO;
+    CREATE TEMPORARY TABLE DT_INICIO(
+        DT_ALUGUEL DATE
+    );
+    DROP TABLE IF EXISTS DT_FIM;
+    CREATE TEMPORARY TABLE DT_FIM(
+        DT_ALUGUEL DATE
+    );
+
+    open dt_aluguel_cursor(current_timestamp);
+    loop
+        fetch dt_aluguel_cursor into DIA_INICIO, DIA_FIM;
+        exit when not found;
+            INSERT INTO DT_INICIO(DT_ALUGUEL)
+            VALUES(DIA_INICIO);
+            INSERT INTO DT_FIM(DT_ALUGUEL)
+            VALUES(DIA_FIM);
+			--RAISE INFO '%', DIA_FIM;
+            --DIA_INICIO = DIA_INICIO || ',' || DIA_INICIO + 1;
+    END LOOP;
+
+    close dt_aluguel_cursor;
+
+    v_alugueis_INCIO = (SELECT ARRAY_AGG(DISTINCT DT_ALUGUEL)
+                        FROM DT_INICIO);
+    v_alugueis_FIM = (SELECT ARRAY_AGG(DISTINCT DT_ALUGUEL)
+                      FROM DT_FIM);
+
+    IF v_alugueis_INCIO IS NULL THEN
+        RETURN '';
+    END IF;
+	--RAISE INFO '%', v_alugueis_INCIO;
+    FOREACH v IN array v_alugueis_INCIO
+        LOOP
+            IF v_aluguel_INICIO IS NOT NULL THEN
+                v_aluguel_INICIO = v_aluguel_INICIO || ',' || v;
+				RAISE INFO '%', v_aluguel_INICIO;
+            ELSE
+                v_aluguel_INICIO = v;
+            END IF;
+
+        end loop;
+    v:=null;
+           FOREACH v IN array v_alugueis_FIM
+    LOOP
+        IF v_aluguel_FIM IS NOT NULL THEN
+            v_aluguel_FIM = v_aluguel_FIM || ',' || v;
+
+        ELSE
+            v_aluguel_FIM = v;
+        END IF;
+
+    end loop;
+    RETURN v_aluguel_INICIO || ';' || v_aluguel_FIM;
 end;
 $$ LANGUAGE PLPGSQL;
