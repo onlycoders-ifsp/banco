cbff7f1520d7d5daba9664ad22c7343bf9eab026
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-01-04 22:23:46 -0300

    Ajustes

--- Functions/FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS.sql
+++ Functions/FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS.sql
@@ -1,19 +1,21 @@
 CREATE OR REPLACE FUNCTION FN_RETORNA_DATAS_ALUGUEIS_EFTUADOS(P_ID_PRODUTO UUID)
     RETURNS TEXT AS $$
-declare DIA_INICIO DATE;
-        DIA_FIM DATE;
-        v_alugueis_INCIO text[];
-        v_alugueis_FIM text[];
-        v_aluguel_INICIO text;
-        v_aluguel_FIM text;
-        v text;
-    declare dt_aluguel_cursor cursor(data timestamp)
+declare --DIA_INICIO DATE;
+        --DIA_FIM DATE;
+        --v_alugueis_INCIO text[];
+        --v_alugueis_FIM text[];
+        --v_aluguel_INICIO text;
+        --v_aluguel_FIM text;
+        --v text;
+		v_alugueis text;
+   /* declare dt_aluguel_cursor cursor(data timestamp)
             for select to_date(to_char(DATA_INICIO,'YYYY-MM-DD'),'YYYY-MM-DD'), to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD')--, extract(day from (DATA_FIM - DATA_INICIO))
                 from alugueis A
                 where A.ID_PRODUTO = UUID(P_ID_PRODUTO)
                 and to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD') >= current_date
-                order by id_aluguel;
+                order by id_aluguel;*/
 begin
+/*
     DROP TABLE IF EXISTS DT_INICIO;
     CREATE TEMPORARY TABLE DT_INICIO(
         DT_ALUGUEL DATE
@@ -22,7 +24,7 @@ begin
     CREATE TEMPORARY TABLE DT_FIM(
         DT_ALUGUEL DATE
     );
-
+*
     open dt_aluguel_cursor(current_timestamp);
     loop
         fetch dt_aluguel_cursor into DIA_INICIO, DIA_FIM;
@@ -66,6 +68,18 @@ begin
         END IF;
 
     end loop;
-    RETURN v_aluguel_INICIO || ';' || v_aluguel_FIM;
+	*/
+	
+	v_alugueis = (select ARRAY_TO_STRING(ARRAY_AGG(to_char(DATA_INICIO,'YYYY-MM-DD')),',') || ';' ||
+					ARRAY_TO_STRING(ARRAY_AGG(to_char(DATA_FIM,'YYYY-MM-DD')),',')
+					from alugueis A
+					where A.ID_PRODUTO = UUID(P_ID_PRODUTO)
+					and to_date(to_char(DATA_FIM,'YYYY-MM-DD'),'YYYY-MM-DD') >= current_date);
+					
+	IF v_alugueis is null then
+		return '';
+	else 
+		RETURN v_alugueis;
+	end if;
 end;
 $$ LANGUAGE PLPGSQL;
